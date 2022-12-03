3e5f0a5b415ac599918f7cdb6aee66b3198c9207
alexrod94 <alexrod94@gmail.com>
2022-12-03 12:08:29 +0000

    Upload everything

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-yellow-300">
+    <h2 class="text-xl text">Hola, {{ name }}</h2>
+    <input type="email" v-model="name" />
+    <p :class="isShow">{{ email }}</p>
+    <br />
+    <br />
+    <button @click="changeShow">Show Email</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const name = ref("Juan");
+const email = ref("alex@ironhack.com");
+const isShow = ref("hidden");
+
+function changeShow() {
+  if (isShow.value === "hidden") {
+    isShow.value = "show";
+  } else {
+    isShow.value = "hidden";
+  }
+}
+</script>
+
+<style>
+.show {
+  display: block;
+}
+
+.hidden {
+  display: none;
+}
+</style>
